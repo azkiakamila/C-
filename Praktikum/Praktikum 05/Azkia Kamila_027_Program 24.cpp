39f80bf95e14e9d969d92e5d7e600449d6494c5b
azkiakamila <90243646+azkiakamila@users.noreply.github.com>
2021-11-14 17:00:52 +0700

    Add files via upload

---
+++ Praktikum/Praktikum 05/Azkia Kamila_027_Program 24.cpp
@@ -0,0 +1,24 @@
+/* Program Duapuluh empat */ 
+/* Program sederhana menggunakan fungsi */ 
+/* yang tidak mengembalikan nilai */
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void garis();
+main()
+{
+	system("cls");
+	garis();
+	printf("NO    NIM   NAMA  NILAI\n");
+	garis();
+	getch();
+
+}
+void garis()
+{
+	printf("========================================================== \n");
+}
+
+
