81c3c4d8450a0cc50382ee7e206f37ab0d5954c3
azkiakamila <90243646+azkiakamila@users.noreply.github.com>
2022-03-09 11:31:09 +0700

    Add files via upload

---
+++ Struktur Data/Praktikum 03/Praktikum03_027_No1.cpp
@@ -0,0 +1,22 @@
+// Azkia Kamila [21091397027]
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int H(const char*s)
+{
+	int x = 0;
+	for (;*s!='\0';s++)
+	++x;
+	return x;
+}
+
+main (void)
+{
+	char String [100];
+	cout << "Tuliskan kata : sisilia thya";
+	cin.getline(String,100);
+	cout << endl;
+	cout << "Jumlah Karakter: 12 " << H(String) << endl;
+}
